a615fa43a3fa5187483c0de917d758db2b871d76
Marc Sabio-Albert <marcsabioalbert@gmail.com>
2025-04-15 14:17:16 +0200

    added pylink to requirements, and created setup_env.bat

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # multipred-eeg-experiment
 Here is the code for the EEG + eye tracking experiment of my multipred project
+
+## Setup
+Windows users must make sure to have python 3.12 installed in PATH. Then, they may run the the setup_env.bat.
---
+++ setup_env.bat
@@ -0,0 +1,14 @@
+@echo off
+
+REM Step 1: Create a virtual environment using Python 3.12
+py -3.12 -m venv .venv
+
+REM Step 2: Activate the virtual environment
+call .venv\Scripts\activate.bat
+
+REM Step 3: Upgrade pip
+python -m pip install --upgrade pip
+
+REM Step 4: Install dependencies
+pip install -r requirements.txt
+
